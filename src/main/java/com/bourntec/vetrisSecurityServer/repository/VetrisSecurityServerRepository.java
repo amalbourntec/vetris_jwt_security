729d371d16d9228295c2d483d9f0ca1d09c65ae8
Donepudi Suresh <suresh@example.com>
2021-10-14 06:29:22 +0530

    code fix for repository and changed db details

--- src/main/java/com/bourntec/vetrisSecurityServer/repository/VetrisSecurityServerRepository.java
+++ src/main/java/com/bourntec/vetrisSecurityServer/repository/VetrisSecurityServerRepository.java
@@ -4,6 +4,8 @@
 package com.bourntec.vetrisSecurityServer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.bourntec.vetrisSecurityServer.model.Users;
 
@@ -11,6 +13,9 @@ import com.bourntec.vetrisSecurityServer.model.Users;
  * @author Donepudi Suresh
  *
  */
+@Repository
 public interface VetrisSecurityServerRepository extends JpaRepository<Users, String> {
-	public Users findOneByLoginId(String loginId);
+	@Query(value="select * from users u where u.login_id=?1",nativeQuery=true)
+	public Users findloginId(String loginId);
+	
 }
